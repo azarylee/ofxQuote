33ee924c77b27c66c39cf2c7f7d8a89481940daa
will <will@globaltllc.com>
2021-03-15 10:52:39 +1100

    optimise amount input

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -190,7 +190,7 @@ export default function() {
                     }}    
                 />  
             </div>
-            <div style={{flex: '0 0 98%', justifyContent: 'center', display: 'flex'}}>
+            <div style={{flex: '1', justifyContent: 'center', display: 'flex'}}>
                 <Button 
                     className="btn roundBtn primary" 
                     text="GET QUOTE" 
